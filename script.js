4c7cb82bc1507b4b03c3b6caf67539231dd4b50d
doaakisha <sugarcane55555@gmail.com>
2024-05-15 03:23:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -8,17 +8,6 @@ yesBtn.addEventListener("click", () => {
   question.innerHTML = "hiii sweetheart <3 happy happy happy birthday waaahhhh !!! my baby turned 22 wo 7alati ya nasi waladi 7abebe galbi 7ayati seed al rujal kibir sana kamla ૮₍ ´ ꒳ `₎ა °｡⋆ *. ̊♡ rabana i5aleek lena w irzogak 3la niyatak always taking care of others before yourself always taking responsibility for everyone always shayil ham al nas gabl nafsak zata and wishing good on everyone no matter what, most peaceful wholesome human ever. i hope you always achieve your goals and is always happy and healthy surrounded by everyone you love. ";
   gif.src =
     "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
-  height:5px;
-  background-size: 50px;
-});
-
-footer {
-   height: 150px;
-   background-image: url(https://i.ibb.co/nPNrBRX/heizou-bc-rp.gif);
-   background-position: centre !important;
-   background-size: cover;
-   box-shadow: calc((400px + (25px * 2)) * 0.025) calc((400px + (25px * 2)) * 0.025) 
-  #8D021F}
 
 noBtn.addEventListener("mouseover", () => {
   const noBtnRect = noBtn.getBoundingClientRect();
